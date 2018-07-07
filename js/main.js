0d14df88bf92476f75462f10007d3f6151632dc6
acho <achoegbunu@gmail.com>
2018-07-07 01:54:46 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var firstName = prompt("what is your first name");
+
+var lastName = prompt("what is your last name");
+
+var realAge = prompt("what is your age in years"); {
+	alert ("Hello," + " " + "my" + " " + "name" + " " + "is" + " " + firstName + " " + lastName + "," + " " + "I" + " " + "am" + " " + realAge*12 + " " + "months" + " " + "old" + " " + "or" + " " + realAge*365 + " " + "days" + " " + "old")
+
+}
+
